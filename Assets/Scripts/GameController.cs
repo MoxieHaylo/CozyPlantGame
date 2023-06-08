b43952a07ff5e2baa50781d00b2e86e44f2d2fd4
Hayley <hayley@moxiehaylo.games>
2023-06-08 10:45:07 +1000

    Created scripts
    Saving
    Inventory
    Potions
    Plants
    GameManager
    Cat
    
    All pseudocode for now

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    /*
+     * Check DayTime
+     * - set lighting 
+     * 
+     * Call weather data
+     * -set background 
+     * 
+     * Run DayTime and weather every 15 minutes of playtime
+     * 
+     * Call inventory data
+     * 
+     * Instantiate plants
+     */
+}
---
+++ Assets/Scripts/SaveSystem.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SaveSystem : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    /*Saving
+     *-----------------
+     * check if save data exists
+     * if no data exists create file
+     * check inventory and record
+     * 
+     * Loading
+     * ----------------
+     * Check to see if file exists
+     * if no file create new
+     * check inventory and set
+     * 
+     * 
+     * 
+     */
+
+
+}
